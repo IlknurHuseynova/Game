be808c6170ed74c3f9ba11adbc8d2aa78d5a8035
IlknurHuseynova <ihuseynova15340@ada.edu.az>
2024-05-04 13:45:00 +0400

    Added something

---
+++ mygame/game.java
@@ -0,0 +1,10 @@
+package mygame;
+public class game{
+    public static int a = 1;
+    public game(int a){
+        game.a = a;
+    }
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+}
